01ef48e7473b1c454dca2edc658874429980992c
Shruthi Kumar <sk593@duke.edu>
2018-12-07 15:34:57 -0500

    help

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -61,13 +61,16 @@ public class HuffProcessor {
 		
 	}
 	private void writeCompressedBits(String[] codings, BitInputStream in, BitOutputStream out) {
-		String code = codings[PSEUDO_EOF];
-		out.writeBits(code.length(), Integer.parseInt(code, 2));
-		
-		for(int k = 0; k < codings.length; k++) {
-			code = codings[k];
+		String code;
+		int val = in.readBits(BITS_PER_WORD);
+		while (val != -1) {
+			code = codings [val];
 			out.writeBits(code.length(), Integer.parseInt(code, 2));
+			val = in.readBits(BITS_PER_WORD);
 		}
+		
+		code = codings[PSEUDO_EOF];
+		out.writeBits(code.length(), Integer.parseInt(code, 2));
 	}
 
 	private void writeHeader(HuffNode root, BitOutputStream out) {
